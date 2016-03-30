427dda1a3e9e225c319ce0f66d3da028034761c0
vidya <vidyacse2008@gmail.com>
2016-03-30 23:16:15 +0530

    change

--- src/Hello.java
+++ src/Hello.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/Hello")
 
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
